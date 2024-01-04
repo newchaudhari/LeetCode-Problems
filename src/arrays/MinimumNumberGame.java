0fae13987bf631c0b846d568b1c99df7df33b086
newchaudhari <chaudharinavin1312@gmail.com>
2024-01-04 16:56:13 +0530

    thousand seperator program

---
+++ src/arrays/MinimumNumberGame.java
@@ -0,0 +1,16 @@
+package arrays;
+
+import java.util.*;
+
+public class MinimumNumberGame {
+    public static void main(String[] args) {
+
+    }
+/*    public static int[] numberGame(int[] nums) {
+        Arrays.sort(nums);
+
+        int[] first=new int[nums.length];
+        Queue<Integer> queue=new PriorityQueue<>();
+        queue.addAll(nums);
+    }*/
+}
--- src/strings/ThousandSeparator.java
+++ src/strings/ThousandSeparator.java
@@ -2,20 +2,37 @@ package strings;
 
 public class ThousandSeparator {
     public static void main(String[] args) {
+        String string = thousandSeparator(130);
+        System.out.println(string);
 
 
     }
+
     public static String thousandSeparator(int n) {
-        String string="";
-        String str1="";
-        if(n<1000){
-            return string+n;
-        }else {
-            StringBuilder stringBuilder=new StringBuilder();
-            stringBuilder.append(n);
-            String string1 = stringBuilder.toString();
-            char[] charArray = string1.toCharArray();
+
+      /*  String str1 = "";
+        if (n < 1000) {
+            return string + n;
+        } else {
+            String string1 = String.valueOf(n);
+            StringBuilder stringBuilder = new StringBuilder(string1);
+            stringBuilder.insert(1, '.');
+            str1 = stringBuilder.toString();
         }
-        return string;
+        return str1;*/
+
+
+        //second approach this is best as time and space complexity
+        String num = String.valueOf(n); //first get num as string
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < num.length(); i++) {//for all digits
+            if (i != 0 && (num.length() - i) % 3 == 0) { //if the digit index divisible to three add separator
+                sb.append(".");
+            }
+            sb.append(num.charAt(i)); //always add the number
+        }
+        return sb.toString();
+
+
     }
 }
