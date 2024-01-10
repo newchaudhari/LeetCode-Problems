440273aaa673cf3d2cfe83fe65bb8300547a5115
newchaudhari <chaudharinavin1312@gmail.com>
2024-01-10 16:18:04 +0530

    split array largest sum

---
+++ src/arrays/DuplicateRotateSortedArray.java
@@ -0,0 +1,84 @@
+package arrays;
+
+public class DuplicateRotateSortedArray {
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+        DuplicateRotateSortedArray rotateSortedArray = new DuplicateRotateSortedArray();
+        System.out.println(rotateSortedArray.search(arr, target));
+    }
+
+    public int search(int[] nums, int target) {
+        int pivot = findPivotWithDuplicates(nums);
+        //if you did not find pivot , it means it is not rotate array
+        if (pivot == -1) {
+            //just do normal binary search
+            return binarySearch(nums, target, 0, nums.length - 1);
+        }
+        //if pivot == target returns pivot
+        if (nums[pivot] == target) {
+            return pivot;
+        }
+        //if target > starting number then start from 0 and end = pivot-1 i.e largest number-1
+        if (target >= nums[0]) {
+            return binarySearch(nums, target, 0, pivot - 1);
+        }
+        //if target < starting number then start from pivot+1 i.e largest number+1 and end = array.length-1
+        return binarySearch(nums, target, pivot + 1, nums.length - 1);
+    }
+
+
+    int binarySearch(int[] nums, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < nums[mid]) {
+                end = mid - 1;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+
+    }
+
+    int findPivotWithDuplicates(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (mid < end && nums[mid] > nums[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && nums[mid] < nums[mid - 1]) {
+                return mid - 1;
+            }
+
+            //if number at start, middle and end are equal then just skip the duplicates
+            if (nums[mid] == nums[start] && nums[mid] == nums[end]) {
+                //skip duplicates
+                //NOTE: what if these elements at start and end were the pivot??
+                //check if start is pivot
+                if (nums[start] > nums[start + 1]) {
+                    return start;
+                }
+                start++;
+                // check if end is pivot
+                if (nums[end] < nums[end - 1]) {
+                    return end - 1;
+                }
+                end--;
+            } else if (nums[start] < nums[mid] || (nums[start] == nums[mid]) && nums[mid] > nums[end]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+
+
+        }
+        return -1;
+    }
+}
---
+++ src/arrays/SplitArrayLargestSum.java
@@ -0,0 +1,46 @@
+package arrays;
+
+public class SplitArrayLargestSum {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+        int k=1;
+        System.out.println(splitArray(arr,k));
+
+    }
+
+    public static int splitArray(int[] nums, int k) {
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < nums.length; i++) {
+            start = Math.max(start, nums[i]); //in the end of loop this will contain maximum item from the array
+            end += nums[i];
+        }
+
+        //binary Search
+        while (start < end) {
+            //try for middle as potential ans
+            int mid = start + (end - start) / 2;
+
+            //calculate how many pieces you can divide this in with this max sum
+            int sum = 0;
+            int pieces = 1;
+            for (int num : nums) {
+                if (sum + num > mid) {
+                    // you cannot add this in this subarray, make new one
+                    //say you add this num in new subarray, then sum = num
+                    sum = num;
+                    pieces++;
+                } else {
+                    sum += num;
+                }
+            }
+
+            if (pieces > k) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
+    }
+}
