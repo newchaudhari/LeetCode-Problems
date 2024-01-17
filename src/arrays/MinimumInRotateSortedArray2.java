e44b56ef95f778baa4044c33f17fa3da65b5806f
newchaudhari <chaudharinavin1312@gmail.com>
2024-01-17 17:24:37 +0530

    Minimum in RSA 2

---
+++ src/arrays/MinimumInRotateSortedArray2.java
@@ -0,0 +1,30 @@
+package arrays;
+
+public class MinimumInRotateSortedArray2 {
+    public static void main(String[] args) {
+        int[] arr = {3, 3, 1, 3};
+        System.out.println(findMin(arr));
+
+    }
+
+    static int findMin(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        if (nums[start] < nums[end]) return nums[start];
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] > nums[end]) {
+                start = mid + 1;
+            } else if (nums[mid] < nums[end]) {
+                end = mid;
+            } else {
+                end--;
+            }
+        }
+
+        return nums[end];
+    }
+}
