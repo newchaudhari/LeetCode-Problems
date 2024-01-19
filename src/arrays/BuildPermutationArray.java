f1285a969bb532c9bc78324a4ca45383394ea45f
newchaudhari <chaudharinavin1312@gmail.com>
2024-01-19 12:05:47 +0530

    buid permutation array

---
+++ src/arrays/BuildPermutationArray.java
@@ -0,0 +1,19 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class BuildPermutationArray {
+    public static void main(String[] args) {
+        int[] arr = {0, 2, 1, 5, 3, 4};
+        System.out.println(Arrays.toString(buildArray(arr)));
+
+    }
+
+    public static int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+}
