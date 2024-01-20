52ded3ffa5c05b6d26327b1069fc674b548cb336
newchaudhari <chaudharinavin1312@gmail.com>
2024-01-20 15:31:24 +0530

    shuffle array

---
+++ src/arrays/ShuffleArray.java
@@ -0,0 +1,37 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.Spliterator;
+
+public class ShuffleArray {
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 1, 3, 4, 7};
+
+        System.out.println(Arrays.toString(shuffle(arr, 3)));
+
+    }
+
+    public static int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[nums.length];
+        int counter = 0;
+        for (int i = 0; i < n; i++) {
+            ans[counter] = nums[i];
+            ans[counter + 1] = nums[i + n];
+            counter += 2;
+        }
+
+        return ans;
+    }
+
+
+    public static int[] shuffle2(int[] nums, int n) {
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < n; i++) {
+            ans[2 * i] = nums[i];
+            ans[2 * i + 1] = nums[i + n];
+        }
+
+        return ans;
+    }
+
+}
