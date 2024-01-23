aa6d024ecb8e55998e1876d8c15dc8ff37032185
newchaudhari <chaudharinavin1312@gmail.com>
2024-01-23 13:43:57 +0530

    Greatest Number of Candies

---
+++ src/arrays/GreatestNumberOfCandies.java
@@ -0,0 +1,34 @@
+package arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GreatestNumberOfCandies {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 1, 3};
+        System.out.println(kidsWithCandies(arr, 3));
+
+    }
+
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> ans = new ArrayList<>();
+        int maxCandy = maxCandyInArray(candies);
+        for (int candy : candies) {
+
+            if ((candy + extraCandies) >= maxCandy) {
+                ans.add(true);
+            } else {
+                ans.add(false);
+            }
+        }
+        return ans;
+    }
+
+    public static int maxCandyInArray(int[] candies) {
+        int maxCandy = 0;
+        for (int i = 0; i < candies.length; i++) {
+            maxCandy = Math.max(maxCandy, candies[i]);
+        }
+        return maxCandy;
+    }
+}
