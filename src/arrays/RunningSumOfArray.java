8666a64adc903d9ecf3b6b0085b8c2c17a205b23
newchaudhari <chaudharinavin1312@gmail.com>
2024-01-24 11:16:18 +0530

    Running Sum of 1d Array

---
+++ src/arrays/RunningSumOfArray.java
@@ -0,0 +1,20 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class RunningSumOfArray {
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 2, 10, 1};
+        System.out.println(Arrays.toString(runningSum(arr)));
+
+    }
+
+    public static int[] runningSum(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            nums[i] = sum;
+        }
+        return nums;
+    }
+}
