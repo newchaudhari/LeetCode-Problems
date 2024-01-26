3cbea8d829e2a1a71097eaa3ecf6c8e9977f646a
newchaudhari <chaudharinavin1312@gmail.com>
2024-01-26 15:05:15 +0530

    Highest Altitudes

---
+++ src/arrays/HighestAltitudes.java
@@ -0,0 +1,20 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class HighestAltitudes {
+    public static void main(String[] args) {
+        int[] arr = {-5, 1, 5, 0, -7};
+        System.out.println(largestAltitude(arr));
+    }
+
+    public static int largestAltitude(int[] gain) {
+        int sum = 0;
+        int altitude = 0;
+        for (int i = 0; i < gain.length; i++) {
+            sum = gain[i] + sum;
+            altitude = Math.max(altitude, sum);
+        }
+        return altitude;
+    }
+}
