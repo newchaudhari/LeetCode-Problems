d2a155a923e8b1c1c6692319b99f77f5bad8cbf3
newchaudhari <chaudharinavin1312@gmail.com>
2024-01-27 16:11:52 +0530

    Words Containing Character

---
+++ src/arrays/WordsContainingCharacter.java
@@ -0,0 +1,43 @@
+package arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class WordsContainingCharacter {
+    public static void main(String[] args) {
+        String[] str = {"leet", "code"};
+        System.out.println(findWordsContaining(str, 'e'));
+
+    }
+
+    // first approach and time complexity of 2ms
+    public static List<Integer> findWordsContaining(String[] words, char x) {
+        List indices = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            char[] chars = words[i].toCharArray();
+            for (int j = 0; j < chars.length; j++) {
+                if (chars[j] == x) {
+                    indices.add(i);
+                    break;
+                }
+            }
+        }
+        return indices;
+    }
+
+    // second approach and best for time complexity of 1ms
+    public static List<Integer> findWordsContaining2(String[] words, char x) {
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            for (int j = 0; j < word.length(); j++) {
+                if (word.charAt(j) == x) {
+                    ans.add(i);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
