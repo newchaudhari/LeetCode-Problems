be861e0ffe78bc14172e883e9f233d715a016a77
newchaudhari <chaudharinavin1312@gmail.com>
2024-02-03 11:37:27 +0530

    Jewels and Stones

--- src/arrays/ContainsDuplicate2.java
+++ src/arrays/ContainsDuplicate2.java
@@ -17,16 +17,14 @@ public class ContainsDuplicate2 {
         //First Approach
 
 //        Arrays.sort(nums);
-        int counter=0;
-        Set<Integer> integers = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (integers.contains(nums[i])){
-                counter++;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 1; j < nums.length-1; j++) {
+                int diff=j-i;
+                if (nums[i] == nums[j] && diff <= k) {
+                    return true;
+                }
             }
-            integers.add(nums[i]);
-
         }
-
         return false;
     }
 }
---
+++ src/strings/JewelsAndStones.java
@@ -0,0 +1,22 @@
+package strings;
+
+//https://leetcode.com/problems/jewels-and-stones/
+public class JewelsAndStones {
+    public static void main(String[] args) {
+        String jewels = "aA";
+        String stones = "aAAbbbb";
+        System.out.println(numJewelsInStones(jewels,stones));
+
+    }
+
+    public static int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        for (int i = 0; i < jewels.length(); i++) {
+            for (int j = 0; j < stones.length(); j++) {
+                if (jewels.charAt(i) == stones.charAt(j)) count++;
+            }
+        }
+        return count;
+    }
+
+}
