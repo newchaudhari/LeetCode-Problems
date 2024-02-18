f6ee1313c68ce10cf2c1f0b56f9fa8c8348e2beb
newchaudhari <chaudharinavin1312@gmail.com>
2024-02-18 10:29:42 +0530

    Number of Segments in a String

---
+++ src/strings/NumberOfSegmentsInString.java
@@ -0,0 +1,47 @@
+package strings;
+
+//https://leetcode.com/problems/number-of-segments-in-a-string/description/
+
+public class NumberOfSegmentsInString {
+    public static void main(String[] args) {
+        String s = ", , , ,        a, eaefa";
+        System.out.println(countSegments2(s));
+
+    }
+
+    // First approach, time complexity is 0ms
+    public static int countSegments(String s) {
+        // Split the string into words using spaces as separators
+        String[] words = s.split(" ");
+        int count = 0;
+
+        // Count non-empty words
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // Second approach, time complexity is 0ms
+    public static int countSegments2(String s) {
+        int count = 0;
+        boolean inSegment = false;
+
+        // Iterate through the characters in the string
+        for (char c : s.toCharArray()) {
+            if (c != ' ') {
+                // If the character is not a space, we are in a segment
+                if (!inSegment) {
+                    inSegment = true;
+                    count++;
+                }
+            } else {
+                // If the character is a space, we are not in a segment
+                inSegment = false;
+            }
+        }
+        return count;
+    }
+}
