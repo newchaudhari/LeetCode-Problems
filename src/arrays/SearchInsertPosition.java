eea3b6725dae22ff674c0cb1cdb7773e73da5b62
newchaudhari <chaudharinavin1312@gmail.com>
2024-03-02 08:40:15 +0530

    modified search insert position

--- src/arrays/SearchInsertPosition.java
+++ src/arrays/SearchInsertPosition.java
@@ -3,18 +3,19 @@ package arrays;
 //https://leetcode.com/problems/search-insert-position/description/
 public class SearchInsertPosition {
     public static void main(String[] args) {
-        int[] array ={1,3,5,6};
-        System.out.println(searchInsert(array,2));
+        int[] array = {1, 3, 5, 6};
+        System.out.println(searchInsert(array, 2));
 
     }
+
     public static int searchInsert(int[] nums, int target) {
-        int index=0;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]==target){
-                index=i;
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                index = i;
                 break;
-            } else if (nums[i]<target) {
-                index=i+1;
+            } else if (nums[i] < target) {
+                index = i + 1;
             }
         }
         return index;
